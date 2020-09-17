a970d405e731b2badbdddc2e842f769e111f76e4
Zaid Ghori <Zaid.ghori1997@gmail.com>
2020-09-17 20:04:03 +0530

    Todo App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css'
+import ListItems from './components/ListItems';
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem:{
+        text:'',
+        key: ''
+      }
+    }
+  }
+
+  setUpdate= (text,key) =>{
+    const items = this.state.items;
+    items.map((item) =>{
+      if(item.key===key){
+        item.text = text;
+      }
+    } )
+    this.setState({
+      items: items
+    })
+   
+}
+
+  deleteItem = (key) => {
+    this.setState({
+    items: this.state.items.filter(function(item){
+     return item.key !== key
+    })
+  })
+  }
+
+  inputHandler= (event) =>{
+    this.setState({
+      currentItem:{
+        text: event.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  
+
+  addItem = (event)=>{
+    event.preventDefault();
+    // console.log(this.state.currentItem);
+    const newItem = this.state.currentItem;
+    if(newItem.text !== ""){
+      const items = [...this.state.items, newItem];
+    
+    this.setState({
+      items: items,
+      // items: [...this.state.items, this.state.currentItem],
+      currentItem :{
+        text:'',
+        key:''
+      }
+      })
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <form className ="app__form">
+        <input placeholder ="Enter your text here" value ={this.state.currentItem.text}
+        onChange={this.inputHandler}/>
+        <button onClick={this.addItem}>Add</button>
+        </form>
+
+        <ListItems setUpdate={this.setUpdate} deleteItem={this.deleteItem} items ={this.state.items}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ListItems.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react';
+import './ListItems.css';
+import FlipMove from 'react-flip-move';
+
+const ListItems = ({items, deleteItem, setUpdate}) =>{
+
+    const completeItem=(item)=>{
+        document.getElementById(item.key).style.textDecoration = "line-through";
+      }
+    return(
+        <div>
+            <FlipMove duration ={300} easing="ease-in-out">
+            {      
+            items.map((item) => (
+                <div className="listitem__list" key ={item.key}>
+                <p>
+                    <input id= {item.key} type="text"
+                     value={item.text}
+                     onChange={
+                         (e)=>{
+                             setUpdate(e.target.value, item.key)
+                         }
+                     }
+                     /> 
+                <span className="listitem__delete" 
+                onClick ={()=> deleteItem(item.key)}>
+                    Delete
+                </span>
+                <div className="listitem__completed" onClick ={()=> completeItem(item)}>Completed </div>
+                </p>
+                
+                </div>
+            ))
+            }
+            </FlipMove>
+        </div>
+    );
+}
+export default ListItems;
