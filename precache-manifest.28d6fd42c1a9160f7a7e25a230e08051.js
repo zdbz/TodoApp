0725aa9874a94889c2eb00b2b229d3a641515cda
Zaid Ghori <Zaid.ghori1997@gmail.com>
2020-09-17 16:06:32 +0530

    Updates

---
+++ precache-manifest.28d6fd42c1a9160f7a7e25a230e08051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df5da37af54abb6230de51715f6641b",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "b378e47fcc468e073e09",
+    "url": "/TodoApp/static/css/main.bf250e4e.chunk.css"
+  },
+  {
+    "revision": "b5d2ec6837776677ec64",
+    "url": "/TodoApp/static/js/2.215db46c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.215db46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b378e47fcc468e073e09",
+    "url": "/TodoApp/static/js/main.0252c498.chunk.js"
+  },
+  {
+    "revision": "c662825f8d338341bcb9",
+    "url": "/TodoApp/static/js/runtime-main.6e726f31.js"
+  }
+]);
